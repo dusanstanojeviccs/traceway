88da4e4f4793c52ffa374cb1e34409778e24964c
ddux <dusan.stanojevic.cs@gmail.com>
2025-12-31 20:55:02 -0600

    added logossvelte fe

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { authState } from '$lib/state/auth.svelte';
+    
+    $effect(() => {
+            if (authState.token) {
+            goto('/issues');
+        } else {
+            goto('/login');
+        }
+    });
+</script>
